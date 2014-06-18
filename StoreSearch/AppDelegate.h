f2ddb70ed4d531961307d1d95a5efecf49a3a279
jiangguimin <jiangguimin@funguide.com.cn>
2014-06-18 10:43:20 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by jiangguimin on 14-6-18.
+//  Copyright (c) 2014年 Dalnpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by jiangguimin on 14-6-18.
+//  Copyright (c) 2014年 Dalnpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
