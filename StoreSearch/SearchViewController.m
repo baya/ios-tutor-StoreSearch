3f4e5b64c5c5d87abdb5583c1dd558ac6110e710
jiangguimin <jiangguimin@funguide.com.cn>
2014-06-27 17:23:12 +0800

    replace networking code with AFNetworking

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -9,6 +9,7 @@
 #import "SearchViewController.h"
 #import "SearchResult.h"
 #import "SearchResultCell.h"
+#import <AFNetworking/AFNetworking.h>
 
 static NSString * const SearchResultCellIdentifier = @"SearchResultCell";
 static NSString * const NothingFoundCellIdentifier = @"NothingFoundCell";
@@ -25,13 +26,14 @@ static NSString * const LoadingCellIdentifier = @"LoadingCell";
 {
     NSMutableArray *_searchResults;
     BOOL _isLoading;
+    NSOperationQueue *_queue;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        _queue = [[NSOperationQueue alloc] init];
     }
     return self;
 }
@@ -166,62 +168,33 @@ static NSString * const LoadingCellIdentifier = @"LoadingCell";
         
         _searchResults = [NSMutableArray arrayWithCapacity:10];
         
-        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         
-        dispatch_async(queue, ^{
-            NSURL *url = [self urlWithSearchText:searchBar.text];
-            NSString *jsonString = [self performStoreRequestWithURL:url];
-            
-            if (jsonString == nil) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self showNetworkError];
-                });
-                return;
-            }
-            
-            NSDictionary *dictionary = [self parseJSON:jsonString];
-            if (dictionary == nil) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [self showNetworkError];
-                });
-                return;
-            }
-            
-            [self parseDictionary:dictionary];
+        NSURL *url = [self urlWithSearchText:searchBar.text];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
+        operation.responseSerializer = [AFJSONResponseSerializer serializer];
+        
+        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject){
+            [self parseDictionary:responseObject];
             [_searchResults sortUsingSelector:@selector(compareName:)];
             
-            dispatch_async(dispatch_get_main_queue(), ^{
-                _isLoading = NO;
-                [self.tableView reloadData];
-            });
-        });
+            _isLoading = NO;
+            [self.tableView reloadData];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+            [self showNetworkError];
+            
+            _isLoading = NO;
+            [self.tableView reloadData];
+        }];
         
+        [_queue addOperation:operation];
+    
     }
 
 }
 
-
-
-- (NSDictionary *)parseJSON:(NSString *)jsonString
-{
-    NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-    
-    NSError *error;
-    id resultObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-    
-    if (resultObject == nil) {
-        NSLog(@"JSON Error: %@", error);
-        return nil;
-    }
-    
-    if (![resultObject isKindOfClass:[NSDictionary class]]){
-        NSLog(@"JSON Error: Expected dictionary");
-        return nil;
-    }
-    
-    return resultObject;
-    
-}
 
 - (void)parseDictionary:(NSDictionary *)dictionary
 {
@@ -345,18 +318,5 @@ static NSString * const LoadingCellIdentifier = @"LoadingCell";
     return UIBarPositionTopAttached;
 }
 
-- (NSString *)performStoreRequestWithURL:(NSURL *)url
-{
-    NSError *error;
-    NSString *resultString = [NSString stringWithContentsOfURL:url
-                                                      encoding:NSUTF8StringEncoding
-                                                         error:&error];
-    if (resultString == nil) {
-        NSLog(@"Download Error: %@", error);
-        return nil;
-    }
-    
-    return resultString;
-}
 
 @end
